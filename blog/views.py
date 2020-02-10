c4315440b68420db9ca27dca6ff582c41ea8a77a
bplatkowki95 <49002606+bplatkowki95@users.noreply.github.com>
2020-02-10 11:24:27 +0100

    edycja profilu, post crud

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Post
 
@@ -36,8 +37,34 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         """ wywolanie na klasie parent """
         return super().form_valid(form)
 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        """ wywolanie na klasie parent """
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+
+        """ kontrola dostepu do edycji posta """
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+
+        """ kontrola dostepu do edycji posta """
+        if self.request.user == post.author:
+            return True
+        return False
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
-
-""" DOKONCZ PostUpdateView i reszte """
